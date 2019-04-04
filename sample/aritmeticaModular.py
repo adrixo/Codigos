28a831dc2c2ac331b9a1b0b860d5fbb614127034
adrixo <adrianvalrom@gmail.com>
2019-04-05 01:49:46 +0200

    funciones

---
+++ sample/aritmeticaModular.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+def suma():
+    return x
+
+def resta():
+    return x
+
+def producto():
+    return x
+
+def division():
+    return x
+
+def invertible():
+    return x
+
+def teoriaChinoRestos():
+    return x
+
+def ecuacionesDiofanticas():
+    return xxx
+
+def indicadorEuler():
+    return x
+
+def teoremaEuler():
+    return x
